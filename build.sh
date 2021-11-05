055cf25d6848f2bc96594b4308adffcea5ed7923
Matt Coleman <matt@datto.com>
2021-11-05 03:53:23 -0400

    initial commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euf -o pipefail
+
+# clean up any existing build artifacts
+rm -rf output
+
+# build
+time kiwi-ng --type iso --debug --temp-dir=${KIWI_TEMP_DIR:-/var/tmp} \
+    system build --description=image-description --target-dir=output 2>&1
---
+++ image-description/root/usr/lib/dracut/modules.d/85create-overlay/module-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+check() {
+    # including a module dedicated to live environments in a host-only initrd doesn't make sense
+    [[ $hostonly ]] && return 1
+    return 255
+}
+
+depends() {
+    echo dmsquash-live
+    return 0
+}
+
+install() {
+    inst_multiple awk basename blkid cat grep mkdir mkfs.ext4 mkfs.xfs mount parted readlink rmdir tr umount
+    # shellcheck disable=SC2154
+    inst_hook pre-udev 25 "$moddir/create-overlay-genrules.sh"
+    inst_script "$moddir/create-overlay.sh" "/sbin/create-overlay"
+    dracut_need_initqueue # Do I need this? I think I do...
+}
